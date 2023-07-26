0fe06a7806848c63155146b79dc7964e9314d030
Tony <yeloboltw@gmail.com>
2023-07-26 12:47:03 -0600

    Converted theme changes to threads
    Trying to improve performance but still getting lag. Asking community.

--- addons/gdtemplate/autoload/global-theme.gd
+++ addons/gdtemplate/autoload/global-theme.gd
@@ -3,6 +3,8 @@ extends Node
 signal new_fontlist
 
 var theme: Theme
+var thread_font: Thread
+var thread_font_size: Thread
 
 #	Needs to consistently match the language list in global user settings.
 var font_list: Dictionary = {
@@ -34,14 +36,36 @@ func get_font_filepath( font_name: String ) -> String:
 	return fonts[ font_name ]
 
 
+func set_font_threaded( filepath: String ) -> void:
+	var loaded_font: Font = load( filepath )
+	for type in theme.get_type_list():
+		theme.set_font( "font", type, loaded_font )
+
+
 func set_font( new_font: String ) -> void:
-	var filepath: String = GlobalTheme.get_font_filepath( new_font )
-	if( filepath != null ):
-		var loaded_font: Font = load( filepath )
-		for type in theme.get_type_list():
-			theme.set_font( "font", type, loaded_font )
+	if( thread_font != null ):
+		thread_font.wait_to_finish()
+	thread_font = Thread.new()
+	#	Retrieves the filepath to the new font.
+	var filepath: String = get_font_filepath( new_font )
+	if( filepath == null ):
+		return
+	#	End defensive return: No filepath found.
+	thread_font.start( set_font_threaded.bind( filepath ) )
+
+
+func set_font_size_threaded( new_size: int ) -> void:
+	for type in theme.get_type_list():
+		theme.set_font_size( "font_size", type, new_size )
 
 
 func set_font_size( new_size: int ) -> void:
-	for type in theme.get_type_list():
-		theme.set_font_size( "font_size", type, new_size )
+	if( thread_font_size != null ):
+		thread_font_size.wait_to_finish()
+	thread_font_size = Thread.new()
+	thread_font_size.start( set_font_size_threaded.bind( new_size ) )
+
+
+func _exit_tree():
+	thread_font.wait_to_finish()
+	thread_font_size.wait_to_finish()
